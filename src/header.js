a27f9a3a620bb685ef1c711b18b16815ad43f2c4
Cory Cook <cory@humanhybrids.io>
2018-09-12 11:49:47 -0700

    add underline to current page

--- src/header.js
+++ src/header.js
@@ -16,6 +16,10 @@ class Header extends HTMLElement {
         text-decoration: none;
         color: inherit;
       }
+      nav[url*="/art" i] > a[href*="/art"],
+      nav[url="/index.html" i] > a[href="/index.html"] {
+        text-decoration: underline;
+      }
     </style>
     <h1><a href="/index.html">humanhybrids</a></h1>
     <slot></slot>
@@ -25,12 +29,19 @@ class Header extends HTMLElement {
     </nav>
     `;
   }
+
+  get nav() {
+    return this.shadowRoot.querySelector("nav");
+  }
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
   }
+
   connectedCallback() {
     this.shadowRoot.appendChild(Header.template.content.cloneNode(true));
+    this.nav.setAttribute("url", window.location.pathname);
   }
 }
 
